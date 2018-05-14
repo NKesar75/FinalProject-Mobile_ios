77d63437f7e7b5a3ec465bdba6c36b8925b00689
Unknown <NKesar@fullsail.edu>
2018-05-14 14:48:06 -0400

    Fixed load issue on Youtube

--- Personal Assistant/Youtube.swift
+++ Personal Assistant/Youtube.swift
@@ -31,42 +31,13 @@ class Youtube: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate,
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setvideo(videoid: "QrVjFfP4pak")
         
-        speechRecognizer.delegate = self
-        SFSpeechRecognizer.requestAuthorization { authStatus in
-            
-            switch authStatus {
-            case .authorized:
-                print("authorized")
-            case .denied:
-                print("not authorized")
-            case .restricted:
-                print("limted authorizion")
-            case .notDetermined:
-                print("no choice yet authorizion")
-            }
-           self.FetchPreviousCall()
-            
-        }
+        self.FetchPreviousCall()
         
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        
-        AVAudioSession.sharedInstance().requestRecordPermission () {
-            [unowned self] allowed in
-            if allowed {
-                // Microphone allowed, do what you like!
-                
-            } else {
-                // User denied microphone. Tell them off!
-                
-            }
-        }
-        //locationManager.stopUpdatingLocation()
     }
     
     func setvideo (videoid:String){
@@ -76,147 +47,7 @@ class Youtube: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate,
     
     
     @IBAction func voicebuttonpressed(_ sender: UIButton) {
-        
-        if audioEngine.isRunning {
-            audioEngine.stop()
-            recognitionRequest?.endAudio()
-            self.stringtoserver = self.stringtoserver!.replacingOccurrences(of: " ", with: "_", options: .literal, range: nil)
-            self.stringtoserver = self.stringtoserver!.replacingOccurrences(of: "\'", with: "", options: .literal, range: nil)
-            print(self.stringtoserver)
-            FetchJSON()
-        } else {
-            startRecording()
-        }
-        
-        
-    }
     
-    func startRecording() {
-    
-    if let recognitionTask = recognitionTask {
-    recognitionTask.cancel()
-    self.recognitionTask = nil
-    }
-    
-    //1. create the session
-    let session = AVAudioSession.sharedInstance()
-    
-    do {
-    // 2. configure the session for recording and playback
-    try session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: .defaultToSpeaker)
-    try session.setActive(true)
-    // 3. set up a high-quality recording session
-    
-    //            let settings = [
-    //                AVFormatIDKey : kAudioFormatLinearPCM,
-    //                AVEncoderAudioQualityKey : AVAudioQuality.high.rawValue,
-    //                AVEncoderBitRateKey: 8000,
-    //                AVNumberOfChannelsKey : 1,
-    //                AVSampleRateKey : 8000
-    //            ] as [String : Any]
-    // 4. create the audio recording, and assign ourselves as the delegate
-    //            audioRecorder = try AVAudioRecorder(url: getAudioFileUrl(), settings: settings)
-    //            audioRecorder?.delegate = self
-    //            audioRecorder?.record()
-    
-    recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
-    
-    let inputNode = audioEngine.inputNode
-    
-    guard let recognitionRequest = recognitionRequest else {
-    fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
-    }
-    
-    recognitionRequest.shouldReportPartialResults = true
-    
-    recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
-    
-    var isFinal = false
-    
-    if result != nil {
-    self.stringtoserver = result?.bestTranscription.formattedString
-    isFinal = (result?.isFinal)!
-    }
-    
-    if error != nil || isFinal {
-    self.audioEngine.stop()
-    inputNode.removeTap(onBus: 0)
-    
-    self.recognitionRequest = nil
-    self.recognitionTask = nil
-    }
-    })
-    
-    let recordingFormat = inputNode.outputFormat(forBus: 0)
-    inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
-    self.recognitionRequest?.append(buffer)
-    }
-    
-    audioEngine.prepare()
-    
-    do {
-    try audioEngine.start()
-    } catch {
-    print("audioEngine couldn't start because of an error.")
-    }
-    
-    //5. Changing recording bool to true
-    isRecording = true
-    }
-    catch let error {
-    // failed to record!
-    }
-    }
-    
-    // Stop recording
-    func finishRecording() {
-        //        audioRecorder?.stop()
-        //        isRecording = false
-        //        let url = getAudioFileUrl()
-        //
-        //        do {
-        //            // AVAudioPlayer setting up with the saved file URL
-        //            let sound = try AVAudioPlayer(contentsOf: url)
-        //            self.player = sound
-        //
-        //            // Here conforming to AVAudioPlayerDelegate
-        //            sound.delegate = self
-        //            sound.prepareToPlay()
-        //            sound.play()
-        //        } catch {
-        //            print("error loading file")
-        //            // couldn't load file :(
-        //        }
-        //        let storage = Storage.storage()
-        //        let storageRef = storage.reference()
-        //
-        //        storageRef.child("ios_voice.amr")
-        //
-    }
-    
-    // Path for saving/retreiving the audio file
-    func getAudioFileUrl() -> URL{
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        let docsDirect = paths[0]
-        let audioUrl = docsDirect.appendingPathComponent("recording.caf")
-        return audioUrl
-    }
-    
-    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
-        if flag {
-            finishRecording()
-        }else {
-            // Recording interrupted by other reasons like call coming, reached time limit.
-        }
-    }
-    
-    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        if flag {
-            
-        }else {
-            // Playing interrupted by other reasons like call coming, the sound has not finished playing.
-        }
-        
     }
     
     
@@ -256,36 +87,14 @@ class Youtube: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate,
                 // add some more check's if for some reason location manager is nil
             }
         })
-        
     }
     
     func FetchPreviousCall(){
-        if Servercalls.serverjson["key"].string != nil {
+        if Servercalls.serverjson["key"].string != nil && Servercalls.serverjson["key"] == "youtube" {
             setvideo(videoid: Servercalls.serverjson["id"].string!)
         }
     }
-    
-    
-     func FetchJSON() {
-
-        let server = Servercalls()
-        server.apicall(city: city!, state: state!, voicecall: self.stringtoserver!)
-        print(Servercalls.serverjson)
-        if Servercalls.serverjson["key"].string != nil {
-            switch (Servercalls.serverjson["key"].string!){
-            case "weather":
-                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Weather_ID") as! Weather
-                    self.present(vc, animated: true, completion: nil)
-            case "youtube":
-               setvideo(videoid: Servercalls.serverjson["id"].string!)
-            case "google":
-                let vc = self.storyboard?.instantiateViewController(withIdentifier: "Search_ID") as! Search
-                self.present(vc, animated: true, completion: nil)
-            default: break
-            }
-        }
-        
-    }
+   
 }
     
 
