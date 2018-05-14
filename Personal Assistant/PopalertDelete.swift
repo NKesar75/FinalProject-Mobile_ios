6cd6d9228a534fed55020031b6e1acdefb750067
Unknown <NKesar@fullsail.edu>
2018-05-14 10:12:39 -0400

    Made animation for list

--- Personal Assistant/PopalertDelete.swift
+++ Personal Assistant/PopalertDelete.swift
@@ -16,6 +16,7 @@ class PopalertDelete: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.black.withAlphaComponent(0.8)
         filenamelabel.text = Make_a_list.nameoflist
+        self.showAnimate()
         // Do any additional setup after loading the view.
     }
 
@@ -40,12 +41,37 @@ class PopalertDelete: UIViewController {
                     // File deleted successfully
                 }
             }
-            self.view.removeFromSuperview()
+            //self.view.removeFromSuperview()
+            self.removeAnimate()
         }
     }
     
     @IBAction func Cancelbutton(_ sender: Any) {
-        self.view.removeFromSuperview()
+        //self.view.removeFromSuperview()
+        self.removeAnimate()
+    }
+    
+    func showAnimate()
+    {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+    }
+    
+    func removeAnimate()
+    {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+        }, completion:{(finished : Bool)  in
+            if (finished)
+            {
+                self.view.removeFromSuperview()
+            }
+        });
     }
     
     /*
