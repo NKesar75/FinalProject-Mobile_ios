8ec230c850b1ce63ec79d7d7df6fe67597e6335a
Nilesh Kesar <NKesar@fullsail.edu>
2018-05-25 12:44:41 -0400

    retrieve the call for weather and put all info in an array for raj

--- PersonalAssistantWatch Extension/WeatherIC.swift
+++ PersonalAssistantWatch Extension/WeatherIC.swift
@@ -19,7 +19,6 @@ class WeatherIC: WKInterfaceController {
         var rain : String
         var templow : String
         var temphigh : String
-        var humidty : String
         var date: String
     }
     
@@ -78,10 +77,23 @@ class WeatherIC: WKInterfaceController {
     
     
     func fetchperviouscall(){
-        if HomePageIC.requestinfo != nil{
-            let weatherinfo = HomePageIC.requestinfo.split(separator: ",")
+        if HomePageIC.requestinfo != "" && HomePageIC.requestinfo != nil{
+            let weatherinfoarray = HomePageIC.requestinfo.split(separator: ",")
+            //0 key //1 city //2 state //3 condition //4 url //5 rain //6 temp low //7 temp high //8 month/date/year //9 repeat condition
             
-            weatherforcasts.append(weatherinfo(Location: weatherinfo[1] + ", " + weatherinfo[2],forcast: "Forcast: " + weatherinfo[3], image:  weatherinfo[4], rain: "Rain: " + weatherinfo[5] + "%", templow: "Low: "  + "°F", temphigh: "High: " + Servercalls.serverjson["results"][0]["temp_highf"].string! + "°F",humidty: "Humidity: " + String(Servercalls.serverjson["results"][0]["humidity"].double!), date: String(Servercalls.serverjson["results"][0]["month"].int!) + "/" + String(Servercalls.serverjson["results"][0]["day"].int!) + "/" + String(Servercalls.serverjson["results"][0]["year"].int!)))
+            weatherforcasts.append(weatherinfo(Location: weatherinfoarray[1] + ", " + weatherinfoarray[2],forcast: "Forcast: " + weatherinfoarray[3], image:  String(weatherinfoarray[4]), rain: "Rain: " + weatherinfoarray[5] + "%", templow: "Low: " + weatherinfoarray[6] + "°F", temphigh: "High: " + weatherinfoarray[7] + "°F", date: String(weatherinfoarray[8])))
+            
+            weatherforcasts.append(weatherinfo(Location: weatherinfoarray[1] + ", " + weatherinfoarray[2],forcast: "Forcast: " + weatherinfoarray[9], image:  String(weatherinfoarray[10]), rain: "Rain: " + weatherinfoarray[11] + "%", templow: "Low: " + weatherinfoarray[12] + "°F", temphigh: "High: " + weatherinfoarray[13] + "°F", date: String(weatherinfoarray[14])))
+            
+            weatherforcasts.append(weatherinfo(Location: weatherinfoarray[1] + ", " + weatherinfoarray[2],forcast: "Forcast: " + weatherinfoarray[15], image:  String(weatherinfoarray[16]), rain: "Rain: " + weatherinfoarray[17] + "%", templow: "Low: " + weatherinfoarray[18] + "°F", temphigh: "High: " + weatherinfoarray[19] + "°F", date: String(weatherinfoarray[20])))
+            
+            weatherforcasts.append(weatherinfo(Location: weatherinfoarray[1] + ", " + weatherinfoarray[2],forcast: "Forcast: " + weatherinfoarray[21], image:  String(weatherinfoarray[22]), rain: "Rain: " + weatherinfoarray[23] + "%", templow: "Low: " + weatherinfoarray[24] + "°F", temphigh: "High: " + weatherinfoarray[25] + "°F", date: String(weatherinfoarray[26])))
+            
+            weatherforcasts.append(weatherinfo(Location: weatherinfoarray[1] + ", " + weatherinfoarray[2],forcast: "Forcast: " + weatherinfoarray[27], image:  String(weatherinfoarray[28]), rain: "Rain: " + weatherinfoarray[29] + "%", templow: "Low: " + weatherinfoarray[30] + "°F", temphigh: "High: " + weatherinfoarray[31] + "°F", date: String(weatherinfoarray[32])))
+            
+            weatherforcasts.append(weatherinfo(Location: weatherinfoarray[1] + ", " + weatherinfoarray[2],forcast: "Forcast: " + weatherinfoarray[33], image:  String(weatherinfoarray[34]), rain: "Rain: " + weatherinfoarray[35] + "%", templow: "Low: " + weatherinfoarray[36] + "°F", temphigh: "High: " + weatherinfoarray[37] + "°F", date: String(weatherinfoarray[38])))
+            
+            weatherforcasts.append(weatherinfo(Location: weatherinfoarray[1] + ", " + weatherinfoarray[2],forcast: "Forcast: " + weatherinfoarray[39], image:  String(weatherinfoarray[40]), rain: "Rain: " + weatherinfoarray[41] + "%", templow: "Low: " + weatherinfoarray[42] + "°F", temphigh: "High: " + weatherinfoarray[43] + "°F", date: String(weatherinfoarray[44])))
         }
         
     }
