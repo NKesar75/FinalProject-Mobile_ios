7244c4d98ca2702dd99bef4d3ae0351edb996961
Nilesh Kesar <NKesar@fullsail.edu>
2018-05-25 12:26:06 -0400

    check to see if issues

--- PersonalAssistantWatch Extension/WeatherIC.swift
+++ PersonalAssistantWatch Extension/WeatherIC.swift
@@ -12,6 +12,19 @@ import Foundation
 
 class WeatherIC: WKInterfaceController {
 
+    struct weatherinfo{
+        var Location : String
+        var forcast : String
+        var image : String
+        var rain : String
+        var templow : String
+        var temphigh : String
+        var humidty : String
+        var date: String
+    }
+    
+    var weatherforcasts:[weatherinfo] = []
+    
     @IBOutlet var weatherTable: WKInterfaceTable!
     
     override func awake(withContext context: Any?) {
@@ -64,20 +77,14 @@ class WeatherIC: WKInterfaceController {
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    func fetchperviouscall(){
+        if HomePageIC.requestinfo != nil{
+            let weatherinfo = HomePageIC.requestinfo.split(separator: ",")
+            
+            weatherforcasts.append(weatherinfo(Location: weatherinfo[1] + ", " + weatherinfo[2],forcast: "Forcast: " + weatherinfo[3], image:  weatherinfo[4], rain: "Rain: " + weatherinfo[5] + "%", templow: "Low: "  + "°F", temphigh: "High: " + Servercalls.serverjson["results"][0]["temp_highf"].string! + "°F",humidty: "Humidity: " + String(Servercalls.serverjson["results"][0]["humidity"].double!), date: String(Servercalls.serverjson["results"][0]["month"].int!) + "/" + String(Servercalls.serverjson["results"][0]["day"].int!) + "/" + String(Servercalls.serverjson["results"][0]["year"].int!)))
+        }
+        
+    }
     
     
     
