88aa83baff403f3047713bc750c016711b030b13
Nilesh Kesar <NKesar@fullsail.edu>
2018-05-31 13:22:26 -0400

    push list and youtube in "rembers"

---
+++ Personal Assistant/Rember.swift
@@ -0,0 +1,35 @@
+//
+//  Rember.swift
+//  Personal Assistant
+//
+//  Created by Hector Kesar on 5/30/18.
+//  Copyright © 2018 Final-Project. All rights reserved.
+//
+
+import UIKit
+
+class Rember: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Personal Assistant/Remberfirebasehelperclass.swift
@@ -0,0 +1,9 @@
+//
+//  Remberfirebasehelperclass.swift
+//  Personal Assistant
+//
+//  Created by Hector Kesar on 5/30/18.
+//  Copyright © 2018 Final-Project. All rights reserved.
+//
+
+import Foundation
