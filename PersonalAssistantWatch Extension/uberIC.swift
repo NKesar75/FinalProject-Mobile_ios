46cf0279903f42059e5027f0fda923ad5b46fd76
Raj Chandan <rpchandan@fullsail.edu>
2018-06-06 16:26:25 -0400

    Added IC for all the features for the watch

---
+++ PersonalAssistantWatch Extension/uberIC.swift
@@ -0,0 +1,31 @@
+//
+//  uberIC.swift
+//  PersonalAssistantWatch Extension
+//
+//  Created by Raj  Chandan on 6/6/18.
+//  Copyright © 2018 Final-Project. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class uberIC: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
