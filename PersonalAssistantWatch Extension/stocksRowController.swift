1b12563712c3b64e1e334ef33bb136c0e0bcd430
Raj Chandan <rpchandan@fullsail.edu>
2018-06-07 13:50:26 -0400

    Added Row Controller for tables

---
+++ PersonalAssistantWatch Extension/stocksRowController.swift
@@ -0,0 +1,13 @@
+//
+//  stocksRowController.swift
+//  PersonalAssistantWatch Extension
+//
+//  Created by Raj  Chandan on 6/7/18.
+//  Copyright © 2018 Final-Project. All rights reserved.
+//
+
+import WatchKit
+
+class stocksRowController: NSObject {
+
+}
